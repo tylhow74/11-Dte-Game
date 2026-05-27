3d82e1a0784c837ff893f4c053a8b95eedb5f471
tylhow74 <tylhow74@kingshigh.school.nz>
2026-05-27 13:16:33 +1200

    Water And Tree

---
+++ 11-dte-game-project/node_2d.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.has_method("_on_water_body_entered"):
+		body._on_water_body_entered(body)
+
+func _on_body_exited(body):
+	if body.has_method("_on_water_body_exited"):
+		body._on_water_body_exited(body)
--- 11-dte-game-project/player.gd
+++ 11-dte-game-project/player.gd
@@ -1,28 +1,49 @@
 extends CharacterBody2D
 
-const SPEED = 200
-const GRAVITY = 900
-const JUMP_FORCE = -350
+const NORMAL_SPEED = 200
+const WATER_SPEED = 100
+
+const NORMAL_GRAVITY = 900
+const WATER_GRAVITY = 200
+
+const NORMAL_JUMP = -350
+const WATER_JUMP = -150
+
+var speed = NORMAL_SPEED
+var gravity = NORMAL_GRAVITY
+var jump_force = NORMAL_JUMP
+
+var in_water = false
 
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
 
+	# Change physics if in water
+	if in_water:
+		speed = WATER_SPEED
+		gravity = WATER_GRAVITY
+		jump_force = WATER_JUMP
+	else:
+		speed = NORMAL_SPEED
+		gravity = NORMAL_GRAVITY
+		jump_force = NORMAL_JUMP
+
 	# Gravity
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		velocity.y += gravity * delta
 
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_FORCE
+		velocity.y = jump_force
 
 	# Movement
 	var direction = Input.get_axis("move_left", "move_right")
 
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	# Flip sprite
 	if direction != 0:
@@ -37,3 +58,13 @@ func _physics_process(delta):
 		anim.play("idle")
 
 	move_and_slide()
+
+
+# WATER DETECTION
+func _on_water_body_entered(body):
+	if body == self:
+		in_water = true
+
+func _on_water_body_exited(body):
+	if body == self:
+		in_water = false
