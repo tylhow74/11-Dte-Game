ab4b6370ca9d7e5d4f906a28939364d83403687b
tylhow74 <tylhow74@kingshigh.school.nz>
2026-07-01 13:17:36 +1200

    v17

---
+++ 11-dte-game-project/blackness.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var dark_overlay = $ColorRect
+
+func _ready():
+	body_entered.connect(player_entered)
+	body_exited.connect(player_left)
+
+func player_entered(body):
+	print("ENTERED:", body.name)
+	if body.name == "Player":
+		dark_overlay.visible = true
+		dark_overlay.color = Color.RED
+
+func player_left(body):
+	if body.name == "Player":
+		dark_overlay.visible = false
