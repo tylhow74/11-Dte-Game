1a8db1b962dee24155df0b93492f5c82abcaa932
tylhow74 <tylhow74@kingshigh.school.nz>
2026-08-11 14:55:53 +1200

    Time down started

--- 11-dte-game-project/Assets/game.gd
+++ 11-dte-game-project/Assets/game.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 var bananas_collected = 0
 var total_bananas = 8
@@ -12,3 +12,25 @@ func collect_banana():
 func win_game():
 	print("YOU WIN!")
 	get_tree().change_scene_to_file("res://winscreen.tscn")
+
+@onready var timer = $Timer
+@onready var label = $Timer/Label
+
+func _ready():
+	timer.start()
+
+func _process(delta):
+	label.text = str(ceil(timer.time_left))
+
+func _on_timer_timeout():
+	var death_screen = get_tree().get_first_node_in_group("death_screen")
+
+	if death_screen == null:
+
+		print("STILL NO DEATH SCREEN (GROUP NOT FOUND)")
+
+		return
+
+	death_screen.show_death_screen()
+
+	get_tree().paused = true
